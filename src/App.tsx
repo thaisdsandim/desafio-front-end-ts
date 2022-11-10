a81d5440f94a3c3f0739140d731e74c68563e470
Thaís Sandim <contatothais.sandim@gmail.com>
2022-11-10 15:44:43 -0400

    feat: import header, search and employees components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-function App() {
+import { Employees } from './components/employees';
+import { Header } from './components/header';
+import { Search } from './components/search';
+import './global.css'
+
+export const App = () => {
   return (
     <>
-
+      <Header />
+      <main>
+        <Search />
+        <Employees />
+      </main>
     </>
   );
 }
-
-export default App
