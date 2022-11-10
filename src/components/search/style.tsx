e9dc786ebdce2edc097fdb0c8e419d3e89606ed2
Thaís Sandim <contatothais.sandim@gmail.com>
2022-11-10 15:46:23 -0400

    feat(search): create search style

---
+++ src/components/search/style.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const EmployeesSearch = styled.div`
+    display: flex;
+    justify-content: space-between;
+  h4 {
+    font-size: 24px;
+    font-weight: 500;
+    color: #1c1c1c;
+  }
+  input {
+    background-color: #ffffff;
+    padding: 8px 0px 8px 12px;
+    border-radius: 4px;
+  }
+  .input {
+    width: 282px;
+    font-size: 16px;
+    font-weight: 400;
+  }
+  .submit-lente {
+    position: absolute;
+    border: none;
+    background: transparent;
+    outline: none;
+    right: 48px;
+    padding-top: 11px;
+    color: #7d7d7d;
+  }
+} `
