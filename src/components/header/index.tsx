e3d2ddd4c5e3feb7d5379c8819ef79098620372b
Thaís Sandim <contatothais.sandim@gmail.com>
2022-11-10 15:43:12 -0400

    feat(header): create header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './style';
+
+export const Header = () => {
+  return (
+    <Container>
+      <img src="public/logo-bemobile.png" alt="Logo Be Mobile" />
+    </Container>
+  )
+}
