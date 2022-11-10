d545476ee2c9c4315b3b6b519ba29bb2764e93aa
Thaís Sandim <contatothais.sandim@gmail.com>
2022-11-10 15:43:22 -0400

    feat(header): create header style

---
+++ src/components/header/style.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: #1c1c1c;
+    img{
+        width: 120px;
+        height: 35px;
+        margin-top: 12px;
+        margin-bottom: 12px;
+        margin-left: 32px;
+        background-color: #1c1c1c;
+    }
+} `
