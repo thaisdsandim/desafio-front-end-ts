a82daee67c4eb30f4a5993e9c941ee2366aa9aad
Thaís Sandim <contatothais.sandim@gmail.com>
2022-11-11 15:51:00 -0400

    feat(app): add api requisition

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
+import { useState, useEffect } from 'react';
 import { Employees } from './components/employees';
 import { Header } from './components/header';
 import { Search } from './components/search';
 import './global.css'
+import axios from 'axios';
 
-export const App = () => {
+function App():any {
+  const [list, setList] = useState([]);
+  const [initial, setInitial] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/employees").then((response) => {
+      setList(response.data);
+      setInitial(response.data);
+    });
+  }, []);
+
   return (
     <>
       <Header />
       <main>
-        <Search />
-        <Employees />
+        <Search list={list} setList={setList} initial={initial} />
+        <Employees list={list} />
       </main>
     </>
   );
-}
+};
+
+export default App;
